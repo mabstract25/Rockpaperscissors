ed76baa662387d52b09f72bb33d5d980ed08aa45
Matt <218104632+mabstract25@users.noreply.github.com>
2025-09-26 11:51:19 +0100

    Fixed return bug

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,28 +1,46 @@
 let humanScore = 0;
 let computerScore = 0;
 
+
+// function playRound(humanChoice, computerChoice) {
+    
+// }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(computerSelection);
+
+// playRound(humanSelection, computerSelection);
+
+
 // Create a new function named getComputerChoice
 function getComputerChoice() {
-    let choice = Math.random();
-    choice = Math.round(choice * 10) / 10
-    console.log(choice);
-    if (choice >= 0.1 && choice <= 0.3) {
-        console.log("Rock");
-    } else if (choice >= 0.4 && choice <= 0.6) {
-        console.log("Paper");
+    compchoice = Math.random();
+    compchoice = Math.round(compchoice * 10) / 10
+    if (compchoice >= 0.1 && compchoice <= 0.3) {
+        console.log("rock");
+        return "rock";
+    } else if (compchoice >= 0.4 && compchoice <= 0.6) {
+        console.log("paper");
+        return "paper";
     } else {
-        console.log("Scissors");
+        console.log("scissors");
+        return "scissors";
     }
-}
+};
 // Function returns a random Rock, Paper or Scissors
 
 // Create a new function named getHumanChoice
 function getHumanChoice() {
-    let choice2 = prompt("Choose Your Weapon: Rock, Paper or Scissors?");
-    console.log(choice2);
-}
+    humchoice = prompt("Choose Your Weapon: Rock, Paper or Scissors?");
+    console.log(humchoice);
+    return humchoice;
+};
 // Prompt method that loads on page load
 // Take whatever score is entered
 // Run computer choice
+
+
 
 
