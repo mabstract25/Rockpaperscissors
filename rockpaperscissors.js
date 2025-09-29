96567a58d54589cfc239f132d33d20b98bc80d05
Matt <218104632+mabstract25@users.noreply.github.com>
2025-09-29 17:06:45 +0100

    Build playgame function
    Also added 5 rounds as duplicates, to be replaced with loops once upskilled.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,49 +1,95 @@
-let humanScore = 0;
-let computerScore = 0;
 
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    if (humanChoice === "paper") {
-        if (computerChoice === "paper") {
-            console.log("DRAW!");
-        }
-        if (computerChoice === "rock") {
-            console.log("WIN!");
-        }
-        if (computerChoice === "scissors") {
-            console.log("LOSE!");
-        }
-    } else if (humanChoice === "rock") {
+function playGame() {
+    
+    let humanScore = 0;
+    let computerScore = 0;
 
-        if (computerChoice === "paper") {
-            console.log("LOSE!");
-        }
-        if (computerChoice === "rock") {
-            console.log("DRAW!");
-        }
-        if (computerChoice === "scissors") {
-            console.log("WIN!");
-        }
-    } else if (humanChoice === "scissors") {
+    
 
-        if (computerChoice === "paper") {
-            console.log("WIN!");
-        }
-        if (computerChoice === "rock") {
-            console.log("LOSE!");
-        }
-        if (computerChoice === "scissors") {
-            console.log("DRAW!");
-        }
-    } else {};
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
+        if (humanChoice === "paper") {
+            if (computerChoice === "paper") {
+                console.log("DRAW!");
+            }
+            if (computerChoice === "rock") {
+                console.log("WIN!");
+                humanScore++;
+            }
+            if (computerChoice === "scissors") {
+                console.log("LOSE!");
+                computerScore++;
+            }
+        } else if (humanChoice === "rock") {
+
+            if (computerChoice === "paper") {
+                console.log("LOSE!");
+                computerScore++;
+            }
+            if (computerChoice === "rock") {
+                console.log("DRAW!");
+            }
+            if (computerChoice === "scissors") {
+                console.log("WIN!");
+                humanScore++;
+            }
+        } else if (humanChoice === "scissors") {
+
+            if (computerChoice === "paper") {
+                console.log("WIN!");
+                humanScore++;
+            }
+            if (computerChoice === "rock") {
+                console.log("LOSE!");
+                computerScore++;
+            }
+            if (computerChoice === "scissors") {
+                console.log("DRAW!");
+            }
+        } else {};
+        console.log("Your Score Is " + humanScore);
+        console.log("Your Opponent's Score Is " + computerScore);
+    } 
+
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+    
+    playRound(humanSelection, computerSelection);
+    // Round 2
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+
+    // Round 3
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+
+    // Round 4
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    
+    // Round 5
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+   
+
+
+
+    if (humanScore > computerScore) {
+            alert("You Win The Game!");
+        } else if (computerScore > humanScore) {
+            alert("You Lose The Game!");
+        } else {
+            alert("It's a Draw!");
     };
 
+};
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
 
 
 // Create a new function named getComputerChoice
@@ -51,13 +97,13 @@ function getComputerChoice() {
     compchoice = Math.random();
     compchoice = Math.round(compchoice * 10) / 10
     if (compchoice >= 0.1 && compchoice <= 0.3) {
-        console.log("rock");
+        console.log("-- Computer chooses: rock --");
         return "rock";
     } else if (compchoice >= 0.4 && compchoice <= 0.6) {
-        console.log("paper");
+        console.log("-- Computer chooses: paper --");
         return "paper";
     } else {
-        console.log("scissors");
+        console.log("-- Computer chooses: scissors --");
         return "scissors";
     }
 };
@@ -66,7 +112,7 @@ function getComputerChoice() {
 // Create a new function named getHumanChoice
 function getHumanChoice() {
     humchoice = prompt("Choose Your Weapon: Rock, Paper or Scissors?");
-    console.log(humchoice);
+    console.log("-- Human chooses: " + humchoice + " --");
     return humchoice;
 };
 // Prompt method that loads on page load
